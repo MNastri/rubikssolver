b7f320f8c570faf68b8a6627758506c27f7ee93a
MNastri <marcellonastri@gmail.com>
2022-08-05 11:33:55 -0300

    feat:cube transformation ABC

---
+++ src/cube_transformations.py
@@ -0,0 +1,18 @@
+from abc import ABC
+from typing import List
+
+
+class CubeTransformation(ABC):
+    def __init__(
+        self,
+        corner_permutation: List = None,
+        corner_orientation: List = None,
+        edge_permutation: List = None,
+        edge_orientation: List = None,
+    ):
+        if not all([corner_permutation, corner_orientation, edge_permutation, edge_orientation]):
+            raise AttributeError
+        self.corner_permutation = corner_permutation
+        self.corner_orientation = corner_orientation
+        self.edge_permutation = edge_permutation
+        self.edge_orientation = edge_orientation
