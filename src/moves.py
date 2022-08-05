5f939eafa2c2277004b853c7d1650aa08d7299fb
MNastri <marcellonastri@gmail.com>
2022-08-05 11:41:52 -0300

    feat:moves enum

---
+++ src/moves.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class Move(Enum):
+    U1 = 0
+    U2 = 1
+    U3 = 2
+    R1 = 3
+    R2 = 4
+    R3 = 5
+    F1 = 6
+    F2 = 7
+    F3 = 8
+    D1 = 9
+    D2 = 10
+    D3 = 11
+    L1 = 12
+    L2 = 13
+    L3 = 14
+    B1 = 15
+    B2 = 16
+    B3 = 17
