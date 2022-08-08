be910707bfba5654a33861c90bb5d07831148db2
MNastri <marcellonastri@gmail.com>
2022-08-08 12:25:42 -0300

    feat:creating cube from a string returns instance

--- src/facelets_cube.py
+++ src/facelets_cube.py
@@ -37,6 +37,7 @@ class FaceletsCube(RubiksCube):
             facelets += (col,)
         assert self._has_all_the_colors(facelets)
         self.facelets = facelets
+        return self
 
     def _has_all_the_colors(self, facelets):
         unique_count = [facelets.count(col) for col in Color]
@@ -47,6 +48,5 @@ if __name__ == "__main__":
     print(FaceletsCube())
     print(FaceletsCube().facelets)
     cube_str = "DUUBULDBFRULBLUFDUBRDFFFBLURBFRRULLLRRBLBDUDLRDBFDFDRF"
-    test_string_cube = FaceletsCube()
-    test_string_cube.from_string(cube_str)
+    test_string_cube = FaceletsCube().from_string(cube_str)
     print(test_string_cube)
