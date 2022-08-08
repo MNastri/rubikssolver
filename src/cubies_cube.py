900cc4cc830da01c71727233ecde0eb04ff5de8e
MNastri <marcellonastri@gmail.com>
2022-08-08 11:15:45 -0300

    docs:add corner orientation multiplication

--- src/cubies_cube.py
+++ src/cubies_cube.py
@@ -60,13 +60,13 @@ class CubieCube(RubiksCube):
         A = "some permutation of corners"
         B = "some permutation of corners"
         M = A * B
-        X[c] = "corner that replaces c in permutation X
+        A[c] = "corner that replaces corner c in permutation A"
         then
         M[c] = A[B[c]]
 
         EXAMPLE: if "Y move = F turn * R turn", what corner replaces UBR? Answer: UFL"
             M[c] = A[B[c]]  →    Y[UBR] = F[R[UBR]]
-          R[UBR] = URF      → F[R[UBR]] = F[URF]
+          R[UBR] = URF      →    Y[UBR] = F[URF]
           F[URF] = UFL      →    Y[UBR] = UFL
         """
         new_corners_permutation = [Corner.URF] * NUMBER_OF_CORNERS
@@ -77,6 +77,22 @@ class CubieCube(RubiksCube):
         return new_corners_permutation
 
     def _multiply_corner_orientation(self, other):
+        """
+        let
+        A = "some permutation of corners"
+        B = "some permutation of corners"
+        M = A * B
+        A[c] = "corner that replaces corner c in permutation A"
+        A[c].ori = "orientation of corner A[c]"
+        then
+        M[c].ori = (A[B[c]].ori + B[c].ori) % 3
+
+        EXAMPLE: if "Y move = F turn * R turn", what is the orientation of the corner
+        that replaces UBR? Answer: 2"
+        M[c].ori = A[B[c]].ori + B[c].ori   →   Y[UBR].ori = F[R[UBR]].ori+R[UBR].ori
+        R[UBR]=UFR  and  R[UBR].ori=1       →   Y[UBR].ori = F[UFR].ori+R[UBR].ori
+        F[UFR].ori=1  and  R[UBR].ori=1     →   Y[UBR].ori = 2
+        """
         new_corners_orientation = [SingleCornerOrientation.normal] * NUMBER_OF_CORNERS
         for cor in Corner:
             corner_in_destination = other.corners_permutation[cor]
