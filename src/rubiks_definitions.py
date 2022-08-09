e2bef85d95f83ef5de0225989348e6669301adb1
MNastri <marcellonastri@gmail.com>
2022-08-09 12:33:28 -0300

    rename:L facelets name

--- src/rubiks_definitions.py
+++ src/rubiks_definitions.py
@@ -3,26 +3,26 @@ from facelets import Facelet as Fac
 
 CORNER_FACELETS = [
     [Fac.U_8_corner, Fac.R_0_corner, Fac.F_2_corner],
-    [Fac.U_6_corner, Fac.F_0_corner, Fac.L_corner1],
-    [Fac.U_0_corner, Fac.L_corner0, Fac.B_corner1],
+    [Fac.U_6_corner, Fac.F_0_corner, Fac.L_2_corner],
+    [Fac.U_0_corner, Fac.L_0_corner, Fac.B_corner1],
     [Fac.U_2_corner, Fac.B_corner0, Fac.R_2_corner],
     [Fac.D_2_corner, Fac.F_8_corner, Fac.R_6_corner],
-    [Fac.D_0_corner, Fac.L_corner3, Fac.F_6_corner],
-    [Fac.D_6_corner, Fac.B_corner3, Fac.L_corner2],
+    [Fac.D_0_corner, Fac.L_8_corner, Fac.F_6_corner],
+    [Fac.D_6_corner, Fac.B_corner3, Fac.L_6_corner],
     [Fac.D_8_corner, Fac.R_8_corner, Fac.B_corner2],
 ]
 EDGE_FACELETS = [
     [Fac.U_5_edge, Fac.R_1_edge],
     [Fac.U_7_edge, Fac.F_1_edge],
-    [Fac.U_3_edge, Fac.L_edge0],
+    [Fac.U_3_edge, Fac.L_1_edge],
     [Fac.U_1_edge, Fac.B_edge0],
     [Fac.D_5_edge, Fac.R_7_edge],
     [Fac.D_7_edge, Fac.F_7_edge],
-    [Fac.D_3_edge, Fac.L_edge3],
+    [Fac.D_3_edge, Fac.L_7_edge],
     [Fac.D_1_edge, Fac.B_edge3],
     [Fac.F_5_edge, Fac.R_3_edge],
-    [Fac.F_3_edge, Fac.L_edge2],
-    [Fac.B_edge2, Fac.L_edge1],
+    [Fac.F_3_edge, Fac.L_5_edge],
+    [Fac.B_edge2, Fac.L_3_edge],
     [Fac.B_edge1, Fac.R_5_edge],
 ]
 CORNER_COLORS = [
