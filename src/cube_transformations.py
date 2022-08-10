5a6c7ceae63fc40df9851f1a029d7557b405ac40
MNastri <marcellonastri@gmail.com>
2022-08-10 10:18:07 -0300

    chore:fix variable/argument names

--- src/cube_transformations.py
+++ src/cube_transformations.py
@@ -5,15 +5,15 @@ from typing import List
 class CubeTransformation(ABC):
     def __init__(
         self,
-        corner_permutation: List = None,
-        corner_orientation: List = None,
-        edge_permutation: List = None,
-        edge_orientation: List = None,
+        corners_permutation: List = None,
+        corners_orientation: List = None,
+        edges_permutation: List = None,
+        edges_orientation: List = None,
     ):
-        if not all([corner_permutation, corner_orientation, edge_permutation, edge_orientation]):
+        if not all([corners_permutation, corners_orientation, edges_permutation, edges_orientation]):
             raise AttributeError
         # TODO check for other errors? it currently only checks if they are not None
-        self.corner_permutation = corner_permutation
-        self.corner_orientation = corner_orientation
-        self.edge_permutation = edge_permutation
-        self.edge_orientation = edge_orientation
+        self.corner_permutation = corners_permutation
+        self.corner_orientation = corners_orientation
+        self.edge_permutation = edges_permutation
+        self.edge_orientation = edges_orientation
