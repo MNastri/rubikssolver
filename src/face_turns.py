2ca0a19d3649282431a33acd012a1b01d8662242
MNastri <marcellonastri@gmail.com>
2022-08-10 12:12:48 -0300

    refactor:subclass CubieCube for all face turns

--- src/face_turns.py
+++ src/face_turns.py
@@ -1,11 +1,6 @@
 from corners import Corner as Co
-from corners import CornersOrientation as CsO
-from corners import CornersPermutation as CsP
 from corners import SingleCornerOrientation as CorOri
-from cube_transformations import CubeTransformation
 from edges import Edge as Ed
-from edges import EdgesOrientation as EsO
-from edges import EdgesPermutation as EsP
 from edges import SingleEdgeOrientation as EdgOri
 from cubies_cube import CubieCube
 
@@ -36,131 +31,131 @@ class UTurn(CubieCube):
         )
 
 
-class RTurn(CubeTransformation):
+class RTurn(CubieCube):
     def __init__(self):
-        corner_permutation = CsP([
+        corner_permutation = [
             Co.DFR, Co.UFL, Co.ULB, Co.URF,
             Co.DRB, Co.DLF, Co.DBL, Co.UBR
-        ])
-        corner_orientation = CsO([
+        ]
+        corner_orientation = [
             CorOri.anticlockwised, CorOri.normal, CorOri.normal, CorOri.clockwised,
             CorOri.clockwised, CorOri.normal, CorOri.normal, CorOri.anticlockwised,
-        ])
-        edge_permutation = EsP([
+        ]
+        edge_permutation = [
             Ed.FR, Ed.UF, Ed.UL, Ed.UB,
             Ed.BR, Ed.DF, Ed.DL, Ed.DB,
             Ed.DR, Ed.FL, Ed.BL, Ed.UR
-        ])
-        edge_orientation = EsO([
+        ]
+        edge_orientation = [
             EdgOri.normal, EdgOri.normal, EdgOri.normal, EdgOri.normal,
             EdgOri.normal, EdgOri.normal, EdgOri.normal, EdgOri.normal,
             EdgOri.normal, EdgOri.normal, EdgOri.normal, EdgOri.normal,
-        ])
-        super(RTurn, self).__init__(
+        ]
+        super().__init__(
             corner_permutation, corner_orientation,
             edge_permutation, edge_orientation
         )
 
 
-class FTurn(CubeTransformation):
+class FTurn(CubieCube):
     def __init__(self):
-        corner_permutation = CsP([
+        corner_permutation = [
             Co.UFL, Co.DLF, Co.ULB, Co.UBR,
             Co.URF, Co.DFR, Co.DBL, Co.DRB
-        ])
-        corner_orientation = CsO([
+        ]
+        corner_orientation = [
             CorOri.clockwised, CorOri.anticlockwised, CorOri.normal, CorOri.normal,
             CorOri.anticlockwised, CorOri.clockwised, CorOri.normal, CorOri.normal,
-        ])
-        edge_permutation = EsP([
+        ]
+        edge_permutation = [
             Ed.UR, Ed.FL, Ed.UL, Ed.UB,
             Ed.DR, Ed.FR, Ed.DL, Ed.DB,
             Ed.UF, Ed.DF, Ed.BL, Ed.BR
-        ])
-        edge_orientation = EsO([
+        ]
+        edge_orientation = [
             EdgOri.normal, EdgOri.fliped, EdgOri.normal, EdgOri.normal,
             EdgOri.normal, EdgOri.fliped, EdgOri.normal, EdgOri.normal,
             EdgOri.fliped, EdgOri.fliped, EdgOri.normal, EdgOri.normal,
-        ])
-        super(FTurn, self).__init__(
+        ]
+        super().__init__(
             corner_permutation, corner_orientation,
             edge_permutation, edge_orientation
         )
 
 
-class DTurn(CubeTransformation):
+class DTurn(CubieCube):
     def __init__(self):
-        corner_permutation = CsP([
+        corner_permutation = [
             Co.URF, Co.UFL, Co.ULB, Co.UBR,
             Co.DLF, Co.DBL, Co.DRB, Co.DFR
-        ])
-        corner_orientation = CsO([
+        ]
+        corner_orientation = [
             CorOri.normal, CorOri.normal, CorOri.normal, CorOri.normal,
             CorOri.normal, CorOri.normal, CorOri.normal, CorOri.normal,
-        ])
-        edge_permutation = EsP([
+        ]
+        edge_permutation = [
             Ed.UR, Ed.UF, Ed.UL, Ed.UB,
             Ed.DF, Ed.DL, Ed.DB, Ed.DR,
             Ed.FR, Ed.FL, Ed.BL, Ed.BR
-        ])
-        edge_orientation = EsO([
+        ]
+        edge_orientation = [
             EdgOri.normal, EdgOri.normal, EdgOri.normal, EdgOri.normal,
             EdgOri.normal, EdgOri.normal, EdgOri.normal, EdgOri.normal,
             EdgOri.normal, EdgOri.normal, EdgOri.normal, EdgOri.normal,
-        ])
-        super(DTurn, self).__init__(
+        ]
+        super().__init__(
             corner_permutation, corner_orientation,
             edge_permutation, edge_orientation
         )
 
 
-class LTurn(CubeTransformation):
+class LTurn(CubieCube):
     def __init__(self):
-        corner_permutation = CsP([
+        corner_permutation = [
             Co.URF, Co.ULB, Co.DBL, Co.UBR,
             Co.DFR, Co.UFL, Co.DLF, Co.DRB
-        ])
-        corner_orientation = CsO([
+        ]
+        corner_orientation = [
             CorOri.normal, CorOri.clockwised, CorOri.anticlockwised, CorOri.normal,
             CorOri.normal, CorOri.anticlockwised, CorOri.clockwised, CorOri.normal,
-        ])
-        edge_permutation = EsP([
+        ]
+        edge_permutation = [
             Ed.UR, Ed.UF, Ed.BL, Ed.UB,
             Ed.DR, Ed.DF, Ed.FL, Ed.DB,
             Ed.FR, Ed.UL, Ed.DL, Ed.BR
-        ])
-        edge_orientation = EsO([
+        ]
+        edge_orientation = [
             EdgOri.normal, EdgOri.normal, EdgOri.normal, EdgOri.normal,
             EdgOri.normal, EdgOri.normal, EdgOri.normal, EdgOri.normal,
             EdgOri.normal, EdgOri.normal, EdgOri.normal, EdgOri.normal,
-        ])
-        super(LTurn, self).__init__(
+        ]
+        super().__init__(
             corner_permutation, corner_orientation,
             edge_permutation, edge_orientation
         )
 
 
-class BTurn(CubeTransformation):
+class BTurn(CubieCube):
     def __init__(self):
-        corner_permutation = CsP([
+        corner_permutation = [
             Co.URF, Co.UFL, Co.UBR, Co.DRB,
             Co.DFR, Co.DLF, Co.ULB, Co.DBL
-        ])
-        corner_orientation = CsO([
+        ]
+        corner_orientation = [
             CorOri.normal, CorOri.normal, CorOri.clockwised, CorOri.anticlockwised,
             CorOri.normal, CorOri.normal, CorOri.anticlockwised, CorOri.clockwised,
-        ])
-        edge_permutation = EsP([
+        ]
+        edge_permutation = [
             Ed.UR, Ed.UF, Ed.UL, Ed.BR,
             Ed.DR, Ed.DF, Ed.DL, Ed.BL,
             Ed.FR, Ed.FL, Ed.UB, Ed.DB
-        ])
-        edge_orientation = EsO([
+        ]
+        edge_orientation = [
             EdgOri.normal, EdgOri.normal, EdgOri.normal, EdgOri.fliped,
             EdgOri.normal, EdgOri.normal, EdgOri.normal, EdgOri.fliped,
             EdgOri.normal, EdgOri.normal, EdgOri.fliped, EdgOri.fliped,
-        ])
-        super(BTurn, self).__init__(
+        ]
+        super().__init__(
             corner_permutation, corner_orientation,
             edge_permutation, edge_orientation
         )
