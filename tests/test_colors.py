84f0c37e3658e816caba2c9f5e23b698ac75c304
MNastri <marcellonastri@gmail.com>
2022-08-13 00:14:58 -0300

    chore:better naming for tests

--- tests/test_colors.py
+++ tests/test_colors.py
@@ -1,29 +1,29 @@
 from rubikssolver import colors
 
 
-def test_number_of_facelets_per_color():
+def test_number_of_facelets_per_color_is_9():
     assert colors.FACELETS_PER_COLOR == 9
 
 
-def test_number_of_colors():
+def test_number_of_colors_is_6():
     assert len(colors.Color) == 6
 
 
 class TestColorMethod:
-    def test_U(self):
+    def test_get_color_U(self):
         assert colors.Color.get_color_from(character="U") == colors.Color.U
 
-    def test_R(self):
+    def test_get_color_R(self):
         assert colors.Color.get_color_from(character="R") == colors.Color.R
 
-    def test_F(self):
+    def test_get_color_F(self):
         assert colors.Color.get_color_from(character="F") == colors.Color.F
 
-    def test_D(self):
+    def test_get_color_D(self):
         assert colors.Color.get_color_from(character="D") == colors.Color.D
 
-    def test_L(self):
+    def test_get_color_L(self):
         assert colors.Color.get_color_from(character="L") == colors.Color.L
 
-    def test_B(self):
+    def test_get_color_B(self):
         assert colors.Color.get_color_from(character="B") == colors.Color.B
