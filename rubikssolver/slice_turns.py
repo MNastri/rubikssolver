8e5f631125e9eb8794299644fa030388534c2190
MNastri <marcellonastri@gmail.com>
2022-08-27 14:35:34 -0300

    style:improve readability

--- rubikssolver/slice_turns.py
+++ rubikssolver/slice_turns.py
@@ -8,53 +8,25 @@ from rubikssolver.edges import SingleEdgeOrientation as EdgOri
 class ETurn(CubieCube):
     def __init__(self):
         corner_permutation = [
-            Co.URF,
-            Co.UFL,
-            Co.ULB,
-            Co.UBR,
-            Co.DFR,
-            Co.DLF,
-            Co.DBL,
-            Co.DRB,
+            Co.URF, Co.UFL, Co.ULB, Co.UBR,
+            Co.DFR, Co.DLF, Co.DBL, Co.DRB
         ]
         corner_orientation = [
-            CorOri.normal,
-            CorOri.normal,
-            CorOri.normal,
-            CorOri.normal,
-            CorOri.normal,
-            CorOri.normal,
-            CorOri.normal,
-            CorOri.normal,
+            CorOri.normal, CorOri.normal, CorOri.normal, CorOri.normal,
+            CorOri.normal, CorOri.normal, CorOri.normal, CorOri.normal,
         ]
         edge_permutation = [
-            Ed.UR,
-            Ed.UF,
-            Ed.UL,
-            Ed.UB,
-            Ed.DR,
-            Ed.DF,
-            Ed.DL,
-            Ed.DB,
-            Ed.FL,
-            Ed.BL,
-            Ed.BR,
-            Ed.FR,
+            Ed.UR, Ed.UF, Ed.UL, Ed.UB,
+            Ed.DR, Ed.DF, Ed.DL, Ed.DB,
+            Ed.FL, Ed.BL, Ed.BR, Ed.FR
         ]
         edge_orientation = [
-            EdgOri.normal,
-            EdgOri.normal,
-            EdgOri.normal,
-            EdgOri.normal,
-            EdgOri.normal,
-            EdgOri.normal,
-            EdgOri.normal,
-            EdgOri.normal,
-            EdgOri.fliped,
-            EdgOri.fliped,
-            EdgOri.fliped,
-            EdgOri.fliped,
+            EdgOri.normal, EdgOri.normal, EdgOri.normal, EdgOri.normal,
+            EdgOri.normal, EdgOri.normal, EdgOri.normal, EdgOri.normal,
+            EdgOri.fliped, EdgOri.fliped, EdgOri.fliped, EdgOri.fliped,
         ]
         super().__init__(
-            corner_permutation, corner_orientation, edge_permutation, edge_orientation
+            corner_permutation, corner_orientation,
+            edge_permutation, edge_orientation
         )
+
