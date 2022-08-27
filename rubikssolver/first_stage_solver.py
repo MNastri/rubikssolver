e2543e08618c562139083cc322811c01d0ae78b6
MNastri <marcellonastri@gmail.com>
2022-08-26 22:48:10 -0300

    feat:first stage solver

---
+++ rubikssolver/first_stage_solver.py
@@ -0,0 +1,43 @@
+from rubikssolver import (
+    cubies_cube,
+    edges,
+)
+
+EDGE_BUFFER = edges.Edge(0)
+SETUP_EDGE = edges.Edge(2)
+
+
+def edge_in_buffer(cube: str):
+    cc = cubies_cube.CubieCube().from_string(cube)
+    buffer = cc.edges_permutation[EDGE_BUFFER]
+    return buffer
+
+
+# def where_is_the_first_piece(cube: str):
+#     edge_to_find = edges.Edge(4)
+#     cc = cubies_cube.CubieCube().from_string(cube)
+#     position = cc.edges_permutation.index(edge_to_find)
+#     return position
+#
+#
+# if __name__ == "__main__":
+#     cube_str = "DUUBULDBFRBFRRULLLBRDFFFBLURDBFDFDRFRULBLUFDURRBLBDUDL"
+#     initial_position = where_is_the_first_piece(cube_str)
+#     edge_in_up_layer = raise_to_up_layer(initial_position)
+#     edge_above_its_destination = rotate_to_destination(edge_in_up_layer)
+#     place_edge_in_destination(edge_above_its_destination)
+#     print(pos)
+#
+#
+# def where_are_the_pieces(cube: str):
+#     edges_to_find = [edges.Edge(val) for val in range(4, 8)]
+#     print(edges_to_find)
+#     cc = cubies_cube.CubieCube().from_string(cube)
+#     print(cc.edges_permutation)
+#     print(cc.edges_permutation.permutated_to_UR)
+#     print([edge for edge in cc.edges_permutation])
+#
+#
+# if __name__ == "__main__":
+#     cube_str = "DUUBULDBFRBFRRULLLBRDFFFBLURDBFDFDRFRULBLUFDURRBLBDUDL"
+#     where_are_the_pieces(cube_str)
