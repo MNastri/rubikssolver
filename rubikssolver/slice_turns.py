9c36b5b5131a9026e573eea5167a2475f98cb554
MNastri <marcellonastri@gmail.com>
2022-08-27 14:53:20 -0300

    feat:add M,S slice turns

--- rubikssolver/slice_turns.py
+++ rubikssolver/slice_turns.py
@@ -30,3 +30,54 @@ class ETurn(CubieCube):
             edge_permutation, edge_orientation
         )
 
+
+class MTurn(CubieCube):
+    def __init__(self):
+        corner_permutation = [
+            Co.URF, Co.UFL, Co.ULB, Co.UBR,
+            Co.DFR, Co.DLF, Co.DBL, Co.DRB
+        ]
+        corner_orientation = [
+            CorOri.normal, CorOri.normal, CorOri.normal, CorOri.normal,
+            CorOri.normal, CorOri.normal, CorOri.normal, CorOri.normal,
+        ]
+        edge_permutation = [
+            Ed.UR, Ed.UB, Ed.UL, Ed.DB,
+            Ed.DR, Ed.UF, Ed.DL, Ed.DF,
+            Ed.FR, Ed.FL, Ed.BL, Ed.BR
+        ]
+        edge_orientation = [
+            EdgOri.normal, EdgOri.fliped, EdgOri.normal, EdgOri.fliped,
+            EdgOri.normal, EdgOri.fliped, EdgOri.normal, EdgOri.fliped,
+            EdgOri.normal, EdgOri.normal, EdgOri.normal, EdgOri.normal,
+        ]
+        super().__init__(
+            corner_permutation, corner_orientation,
+            edge_permutation, edge_orientation
+        )
+
+
+class STurn(CubieCube):
+    def __init__(self):
+        corner_permutation = [
+            Co.URF, Co.UFL, Co.ULB, Co.UBR,
+            Co.DFR, Co.DLF, Co.DBL, Co.DRB
+        ]
+        corner_orientation = [
+            CorOri.normal, CorOri.normal, CorOri.normal, CorOri.normal,
+            CorOri.normal, CorOri.normal, CorOri.normal, CorOri.normal,
+        ]
+        edge_permutation = [
+            Ed.UL, Ed.UF, Ed.DL, Ed.UB,
+            Ed.UR, Ed.DF, Ed.DR, Ed.DB,
+            Ed.FR, Ed.FL, Ed.BL, Ed.BR
+        ]
+        edge_orientation = [
+            EdgOri.fliped, EdgOri.normal, EdgOri.fliped, EdgOri.normal,
+            EdgOri.fliped, EdgOri.normal, EdgOri.fliped, EdgOri.normal,
+            EdgOri.normal, EdgOri.normal, EdgOri.normal, EdgOri.normal,
+        ]
+        super().__init__(
+            corner_permutation, corner_orientation,
+            edge_permutation, edge_orientation
+        )
