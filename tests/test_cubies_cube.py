bbe46c3c6b9b3d33acd1c2904a36912dc8b2c87c
MNastri <marcellonastri@gmail.com>
2022-08-28 00:47:27 -0300

    test:cubies_cube to string method

--- tests/test_cubies_cube.py
+++ tests/test_cubies_cube.py
@@ -4,7 +4,7 @@ from rubikssolver import (
 )
 
 
-class TestToString:
+class TestToStringMethod:
     solved_cube = cubies_cube.CubieCube()
 
     def test_solved_cube(self):
@@ -14,8 +14,37 @@ class TestToString:
         )
 
     def test_u_turn(self):
-
         assert (
             face_turns.UTurn().to_string()
             == "UUUUUUUUUBBBRRRRRRRRRFFFFFFDDDDDDDDDFFFLLLLLLLLLBBBBBB"
+        )
+
+    def test_r_turn(self):
+        assert (
+            face_turns.RTurn().to_string()
+            == "UUFUUFUUFRRRRRRRRRFFDFFDFFDDDBDDBDDBLLLLLLLLLUBBUBBUBB"
+        )
+
+    def test_f_turn(self):
+        assert (
+            face_turns.FTurn().to_string()
+            == "UUUUUULLLURRURRURRFFFFFFFFFRRRDDDDDDLLDLLDLLDBBBBBBBBB"
+        )
+
+    def test_d_turn(self):
+        assert (
+            face_turns.DTurn().to_string()
+            == "UUUUUUUUURRRRRRFFFFFFFFFLLLDDDDDDDDDLLLLLLBBBBBBBBBRRR"
+        )
+
+    def test_l_turn(self):
+        assert (
+            face_turns.LTurn().to_string()
+            == "BUUBUUBUURRRRRRRRRUFFUFFUFFFDDFDDFDDLLLLLLLLLBBDBBDBBD"
+        )
+
+    def test_b_turn(self):
+        assert (
+            face_turns.BTurn().to_string()
+            == "RRRUUUUUURRDRRDRRDFFFFFFFFFDDDDDDLLLULLULLULLBBBBBBBBB"
         )
