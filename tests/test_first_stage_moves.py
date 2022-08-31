b63ad06910ad16b1b978d3f6d319744414e7ecc5
MNastri <marcellonastri@gmail.com>
2022-08-31 00:22:03 -0300

    test:find setup move method

--- tests/test_first_stage_moves.py
+++ tests/test_first_stage_moves.py
@@ -3,6 +3,7 @@ from rubikssolver.edges import (
     SingleEdgeOrientation,
 )
 from rubikssolver.first_stage_moves import Puzzle
+from rubikssolver.moves import Move
 
 
 class TestIsBufferEdgeCorrectMethod:
@@ -13,8 +14,11 @@ class TestIsBufferEdgeCorrectMethod:
         assert puzzle._is_buffer_edge_correct(edge, orientation) == True
 
 
-# class TestFindSetupMoveMethod:
-#     def test_buffer_has_dr_normal_oriented_then_setup_is_d2l2(self):
-#         edge = Edge.DR
-#         orientation = SingleEdgeOrientation.normal
-#         assert find_setup_moves(edge, orientation) == "D2L2"
+class TestFindSetupMoveMethod:
+    def test_buffer_has_dr_normal_oriented_then_setup_is_d2l2(self):
+        puzzle = Puzzle().from_string(
+            "UUUUUUUUUBLFRRRRRRFFRFFFFFFDDDDDDDDDLRLLLLLLLRBBBBBBBB"
+        )
+        edge = Edge.DR
+        orientation = SingleEdgeOrientation.normal
+        assert puzzle.find_setup_moves(edge, orientation) == [Move.D2, Move.L2]
