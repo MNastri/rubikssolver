5ce5f3a51ee3a8208f3905fcc7a87b2736da31e6
MNastri <marcellonastri@gmail.com>
2022-09-04 16:44:43 -0300

    chore:group tests

--- tests/test_first_stage_solver.py
+++ tests/test_first_stage_solver.py
@@ -18,22 +18,21 @@ def test_setup_edge_is_ul():
     assert SETUP_EDGE == Edge.UL
 
 
-def test_buffer_is_ul_edge_with_normal_orienation():
-    one_edge_to_go = "UUUUUUUUUBLFRRRRRRFFRFFFFFFDDDDDDDDDLRLLLLLLLRBBBBBBBB"
-    cube = FirstStageSolver().from_string(one_edge_to_go)
-    assert cube.edge_in_buffer() == (Edge.UL, SingleEdgeOrientation.normal)
+class TestEdgeInBuffer:
+    def test_buffer_edge_is_ul_with_normal_orienation(self):
+        one_edge_to_go = "UUUUUUUUUBLFRRRRRRFFRFFFFFFDDDDDDDDDLRLLLLLLLRBBBBBBBB"
+        cube = FirstStageSolver().from_string(one_edge_to_go)
+        assert cube.edge_in_buffer() == (Edge.UL, SingleEdgeOrientation.normal)
 
+    def test_buffer_edge_is_ul_with_fliped_orienation(self):
+        one_edge_to_go = "UUURULUUUBUFRRRRRRFFRFFFFFFDDDDDDDDDLULLLLLLLRBBBBBBBB"
+        cube = FirstStageSolver().from_string(one_edge_to_go)
+        assert cube.edge_in_buffer() == (Edge.UL, SingleEdgeOrientation.fliped)
 
-def test_buffer_is_ul_edge_with_fliped_orienation():
-    one_edge_to_go = "UUURULUUUBUFRRRRRRFFRFFFFFFDDDDDDDDDLULLLLLLLRBBBBBBBB"
-    cube = FirstStageSolver().from_string(one_edge_to_go)
-    assert cube.edge_in_buffer() == (Edge.UL, SingleEdgeOrientation.fliped)
-
-
-def test_buffer_is_uf_edge_with_normal_orienation():
-    u_prime_move = "UUUUUUUUUFFFRRRRRRLLLFFFFFFDDDDDDDDDBBBLLLLLLRRRBBBBBB"
-    cube = FirstStageSolver().from_string(u_prime_move)
-    assert cube.edge_in_buffer() == (Edge.UF, SingleEdgeOrientation.normal)
+    def test_buffer_edge_is_uf_with_normal_orienation(self):
+        u_prime_move = "UUUUUUUUUFFFRRRRRRLLLFFFFFFDDDDDDDDDBBBLLLLLLRRRBBBBBB"
+        cube = FirstStageSolver().from_string(u_prime_move)
+        assert cube.edge_in_buffer() == (Edge.UF, SingleEdgeOrientation.normal)
 
 
 class TestIsBufferEdgeCorrectMethod:
